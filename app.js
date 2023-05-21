6d23b3d35bc1fef0bfe98266857994c7e57737be
tihynihy <tihynihy@gmail.com>
2023-05-21 23:13:29 +0200

    The following changes were made: - Imported the required dependencies: axios, readline, and fs. - Created a readline interface for reading user input from the console. - Defined a helper function, prompt(), which returns a promise to handle user prompts asynchronously. - Implemented the login() function, which handles the login process:   - Prompts the user for their username and password.   - Reads the stored passwords from a JSON file.   - Checks if the entered username and password match the stored credentials.   - If the login is successful, proceeds with weather data retrieval.   - If the login fails, displays an error message. - Defined options for history, forecast, and real-time API requests, including the API endpoint, query parameters, and headers. - Used axios to send HTTP requests to the WeatherAPI and handle the responses. - Logged the retrieved weather data to the console. - Closed the readline interface after completing the login process.
    Note: The API key and host headers used in the code are specific to the RapidAPI WeatherAPI. Make sure to replace them with valid credentials if using a different weather API.
    
    This implementation enables users to log in and retrieve weather information based on their input. The code can be further enhanced by adding error handling, input validation, and additional functionality as required.

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
+const axios = require('axios');
 const readline = require('readline');
-const login = require('./login');
-const getHistory = require('./history');
-const getForecast = require('./forecast');
-const getRealtime = require('./realtime');
+const fs = require('fs');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -17,22 +15,94 @@ function prompt(question) {
   });
 }
 
-async function main() {
-  const loggedIn = await login();
+async function login() {
 
-  if (!loggedIn) {
+  const username = await prompt('Enter your username: ');
+  const password = await prompt('Enter your password: ');
+
+ 
+  let passwords;
+  try {
+    passwords = JSON.parse(fs.readFileSync('local_db.json'));
+  } catch (error) {
+    console.error('Error reading passwords:', error);
     rl.close();
     return;
   }
 
-  const city = await prompt('Enter the city: ');
-  const date = await prompt('Enter the date (YYYY-MM-DD): ');
+ 
+  if (passwords[username] === password) {
+    console.log('Login successful.');
 
-  await getHistory(city, date);
-  await getForecast(city);
-  await getRealtime(city);
+
+    const city = await prompt('Enter the city: ');
+    const date = await prompt('Enter the date (YYYY-MM-DD): ');
+
+ 
+    const historyOptions = {
+      method: 'GET',
+      url: 'https://weatherapi-com.p.rapidapi.com/history.json',
+      params: {
+        q: city,
+        dt: date,
+        lang: 'en'
+      },
+      headers: {
+        'X-RapidAPI-Key': 'ffbdc38f00mshfa60716f35785ffp10b986jsn56a8db74c3e2',
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const historyResponse = await axios.request(historyOptions);
+      console.log(historyResponse.data);
+    } catch (error) {
+      console.error(error);
+    }
+
+    
+    const forecastOptions = {
+      method: 'GET',
+      url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
+      params: {
+        q: city,
+        days: '3'
+      },
+      headers: {
+        'X-RapidAPI-Key': 'ffbdc38f00mshfa60716f35785ffp10b986jsn56a8db74c3e2',
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const forecastResponse = await axios.request(forecastOptions);
+      console.log(forecastResponse.data);
+    } catch (error) {
+      console.error(error);
+    }
+
+ 
+    const realtimeOptions = {
+      method: 'GET',
+      url: 'https://weatherapi-com.p.rapidapi.com/current.json',
+      params: { q: city },
+      headers: {
+        'X-RapidAPI-Key': 'ffbdc38f00mshfa60716f35785ffp10b986jsn56a8db74c3e2',
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const realtimeResponse = await axios.request(realtimeOptions);
+      console.log(realtimeResponse.data);
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    console.log('Invalid username or password. Login failed.');
+  }
 
   rl.close();
 }
 
-main();
+login();
